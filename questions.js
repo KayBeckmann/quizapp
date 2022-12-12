755ab66b59761c9aadbe106feef6f8a378112af0
Kay <kay@hp>
2022-12-12 18:53:29 +0100

    question json hinzugefügt.

---
+++ questions.js
@@ -0,0 +1,8 @@
+const questions = [
+  {
+    category: "html",
+    qusestion: "Wer hat HTML erfunden?",
+    answers:["Lady Gaga", "Tim Burton", "Homer Simpson", "DIE RICHTIGE ANTWORT"],
+    right_answer:4
+  },
+];
