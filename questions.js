c1fbf82a3f5c91c079d404315610082f8163a6b2
Kay Beckmann <info@beckmann-md.de>
2022-12-14 21:24:34 +0100

    Fragen ergänzt

--- questions.js
+++ questions.js
@@ -2,7 +2,40 @@ const questions = [
   {
     category: "html",
     qusestion: "Wer hat HTML erfunden?",
-    answers:["Lady Gaga", "Tim Burton", "Homer Simpson", "DIE RICHTIGE ANTWORT"],
-    right_answer:4
+    answers:["Lady Gaga", "Tim Bernes-Lee", "Homer Simpson", "Justin Bieber"],
+    right_answer:1
+  },
+  {
+    category: "html",
+    qusestion: "Was bedeutet das HTML Tag &lt;a&gt;?",
+    answers:["Text Fett", "Container", "Ein Link", "Kursiv"],
+    right_answer:2
+  },
+  {
+    category: "html",
+    qusestion: "Wie bindet man eine Website in eine Website ein?",
+    answers:["&lt;iframe&gt;, &lt;frame&gt;, and &lt;frameset&gt;",
+     "&lt;iframe&gt;",
+     "&lt;frame&gt;",
+     "&lt;frameset&gt;"],
+    right_answer:1
+  },
+  {
+    category: "html",
+    qusestion: "Wer hat HTML erfunden?",
+    answers:["Lady Gaga", "Tim Bernes-Lee", "Homer Simpson", "Justin Bieber"],
+    right_answer:1
+  },
+  {
+    category: "html",
+    qusestion: "Wer hat HTML erfunden?",
+    answers:["Lady Gaga", "Tim Bernes-Lee", "Homer Simpson", "Justin Bieber"],
+    right_answer:1
+  },
+  {
+    category: "html",
+    qusestion: "Wer hat HTML erfunden?",
+    answers:["Lady Gaga", "Tim Bernes-Lee", "Homer Simpson", "Justin Bieber"],
+    right_answer:1
   },
 ];
