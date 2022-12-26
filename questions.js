b19dc5d1f03e3e2d2b65c7ac41be44570398c443
Kay Beckmann <info@beckmann-md.de>
2022-12-26 20:07:12 +0100

    Fragen überarbeitet

--- questions.js
+++ questions.js
@@ -21,21 +21,21 @@ const questions = [
     right_answer:1
   },
   {
-    category: "html",
-    qusestion: "Wer hat HTML erfunden?",
-    answers:["Lady Gaga", "Tim Bernes-Lee", "Homer Simpson", "Justin Bieber"],
-    right_answer:1
+    category: "css",
+    qusestion: "Wie ändert man die Textfarbe?",
+    answers:["color: rgb(25, 48, 96)", "color: rbg(25, 48, 96)", "color = rgb(25, 48, 96)", "text-color: rgb(25, 48, 96)"],
+    right_answer:0
   },
   {
-    category: "html",
-    qusestion: "Wer hat HTML erfunden?",
-    answers:["Lady Gaga", "Tim Bernes-Lee", "Homer Simpson", "Justin Bieber"],
-    right_answer:1
+    category: "css",
+    qusestion: "Wie declariert man ein Flex-Grid?",
+    answers:["display = flex", "display(flex)", "display: none", "display: flex"],
+    right_answer:3
   },
   {
-    category: "html",
-    qusestion: "Wer hat HTML erfunden?",
-    answers:["Lady Gaga", "Tim Bernes-Lee", "Homer Simpson", "Justin Bieber"],
-    right_answer:1
+    category: "css",
+    qusestion: "Wie gestaltet man eine Mobile-Ansicht?",
+    answers:["media (max-width: 800px)", "media: max-width=800px", "@media (max-width: 800px)", "@media = max-width(800px)"],
+    right_answer:2
   },
 ];
